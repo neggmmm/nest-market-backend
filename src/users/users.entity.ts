649a570a748b2086a68ed04bc35cdabb2c532fbb
neggmmm <kekkdead@gmail.com>
2026-03-22 21:58:22 +0200

    add users module (Basic CRUD)

---
+++ src/users/users.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User{
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column()
+    name:string;
+
+    @Column({unique:true})
+    email:string;
+
+    @Column()
+    role:string;
+
+    @Column({select:false})
+    password:string;
+}
